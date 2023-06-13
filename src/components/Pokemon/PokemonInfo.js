eb7a8ea85f143975397642e3638c9686655c2a2a
Maryna Khmarska <myhappyday@ukr.net>
2023-06-13 22:13:26 +0300

    Module 3 Lesson 6.3.

--- src/components/Pokemon/PokemonInfo.js
+++ src/components/Pokemon/PokemonInfo.js
@@ -4,19 +4,19 @@ import PokemonErrorView from './PokemonErrorView';
 import PokemonPendingView from './PokemonPendingView';
 import pokemonAPI from '../../services/pokemon-api';
 
-// const Status = {
-//   IDLE: 'idle',
-//   PENDING: 'pending',
-//   RESOLVED: 'resolved',
-//   REJECTED: 'rejected',
-// };
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
 
 export default class PokemonInfo extends Component {
   state = {
     pokemon: null,
     error: null,
-    status: 'idle',
-    // status: Status.IDLE,
+    // status: 'idle',
+    status: Status.IDLE,
     // loading: false,
   };
 
@@ -63,27 +63,22 @@ export default class PokemonInfo extends Component {
       //     .catch(error => this.setState({ error, status: 'rejected' }));
       // }, 1000);
 
-    this.setState({ status: 'pending' });
+      // this.setState({ status: 'pending' });
+      // setTimeout(() => {
+      //   pokemonAPI
+      //     .fetchPokemon(nextName)
+      //     .then(pokemon => this.setState({ pokemon, status: 'resolved' }))
+      //     .catch(error => this.setState({ error, status: 'rejected' }));
+      // }, 1000);
+
+      this.setState({ status: Status.PENDING });
       setTimeout(() => {
         pokemonAPI
           .fetchPokemon(nextName)
-          .then(pokemon => this.setState({ pokemon, status: 'resolved' }))
-          .catch(error => this.setState({ error, status: 'rejected' }));
-      }, 1000);
-
+          .then(pokemon => this.setState({ pokemon, status: Status.RESOLVED }))
+          .catch(error => this.setState({ error, status: Status.REJECTED }));
+      }, 3000);
     }
-
-    // const prevName = prevProps.pokemonName;
-    // const nextName = this.props.pokemonName;
-    // if (prevName !== nextName) {
-    //   this.setState({ status: Status.PENDING });
-    //   setTimeout(() => {
-    //     pokemonAPI
-    //       .fetchPokemon(nextName)
-    //       .then(pokemon => this.setState({ pokemon, status: Status.RESOLVED }))
-    //       .catch(error => this.setState({ error, status: Status.REJECTED }));
-    //   }, 3000);
-    // }
   }
 
   render() {
